15e39e49b1c9c716a41f26223cf92cdb3ca9bffe
RodrigoRamirez-N <rodry2810g@gmail.com>
2025-04-05 14:26:22 -0600

    Ya metí el spValidarUsuario xd

--- sql/SP/spAuthLogin.sql
+++ sql/SP/spAuthLogin.sql
@@ -5,3 +5,5 @@ CALL spValidarUsuario("nombre1@gmail.com" , "contrasenia1", @nombre);
 SELECT @nombre;
 
 SHOW CREATE PROCEDURE spValidarUsuario;
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spValidarUsuario`(IN _email VARCHAR(500), IN _contrasena VARCHAR(500), OUT _nombre VARCHAR(500)) BEGIN SELECT nombre INTO _nombre from examen_ingles_usuario where email = _email and contrasena = _contrasena; END
